e2891e1f89dfb0ce9b93ef0eb423a2ec33ae9aba
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-03-10 20:50:44 +0000

    update

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,8 +1,7 @@
-// Wait for the DOM to be fully loaded
-    document.addEventListener("DOMContentLoaded", function () {
-        // Set a timeout to hide the loading screen after 4 seconds (4000 milliseconds)
-        setTimeout(function () {
-            var loadingScreen = document.getElementById("loading-screen");
-            loadingScreen.style.opacity = "0"; // Set opacity to 0 for a fade-out effect
-        }, 5000);
-    });
+// document.addEventListener("DOMContentLoaded", function () {
+//     setTimeout(function () {
+//         var loadingScreen = document.getElementById("loading-screen");
+//         loadingScreen.style.opacity = "0";
+//         loadingScreen.style.display = "none";
+//     }, 5000);
+// });

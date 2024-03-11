53cda87a30f818b4d9c064713ecde5b4ca0f563a
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-03-11 23:01:35 +0000

    update

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        var loadingScreen = document.getElementById("loading-screen");
-        loadingScreen.style.opacity = "0";
-        loadingScreen.style.display = "none";
-    }, 5000);
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     setTimeout(function () {
+//         var loadingScreen = document.getElementById("loading-screen");
+//         loadingScreen.style.opacity = "0";
+//         loadingScreen.style.display = "none";
+//     }, 5000);
+// });
+
+

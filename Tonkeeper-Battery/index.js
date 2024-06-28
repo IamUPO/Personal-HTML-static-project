09a47e7649c2aefd5a5716cd41b06ec940959551
PROMISE UZOR OKWUDIRI <158743974+iamupo@users.noreply.github.com>
2024-06-28 16:30:10 +0000

    update

---
+++ Tonkeeper-Battery/index.js
@@ -0,0 +1,10 @@
+
+/* Pinegrow generated Interactions Begin */
+
+if(typeof pgia == 'undefined') {
+    console && console.error('Pinegrow Interactions: pgia.js must be included before this script.');
+} else {
+    pgia.update();
+}
+
+    /* Pinegrow generated Interactions End */
